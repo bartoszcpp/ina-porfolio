c0bef745c5475922fcd789bdaac6de9a66134cbd
Bartosz Ciąpała <bartosz.cp@gmail.com>
2021-06-24 14:51:11 +0200

    feat: create page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import "bootstrap/dist/css/bootstrap.css"
+import "./index.css"
+import Header from "./components/Header"
+import Main from "./components/Main"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
 }
